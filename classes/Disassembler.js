ef1613dd00087cd080211e14b9b6984ea6444a6f
Tania Rascia <taniarascia@gmail.com>
2019-01-07 19:54:04 -0600

    Fix incorrect instructions and format function

--- classes/Disassembler.js
+++ classes/Disassembler.js
@@ -11,39 +11,59 @@ class Disassembler {
   }
 
   format(decodedInstruction) {
+    // A = Address
+    // N, NN = Constant
+    // R = Register
+    // K = Key
+    // A = Address
+    // F = Fake
+    // I, [I] = Implied
+    // DT = Delay Timer
+    // ST = Sount Timer
+    // B = BCD
+    // DW = Data Word
+
     let types = decodedInstruction.instruction.arguments.map(arg => arg.type)
-    let args = decodedInstruction.args
-    let argsLookup = types.map((type, i) => [ type, args[i] ]) // zip it
-
-    argsLookup.forEach((type, i) => {
-      if (type[0] === 'K' || type[0] === 'KK' || type[0] === 'KKK') {
-        argsLookup[i].push(`v${type[1].toString(16)}`)
-      } else {
-        argsLookup[i].push(type[0] + type[1].toString(16))
-      }
-    })
-
+    let rawArgs = decodedInstruction.args
     let formatted
 
-    if (decodedInstruction.args.length > 0) {
-      formatted =
-        decodedInstruction.instruction.name + ' ' + argsLookup.map(arg => arg[2]).join(', ')
+    if (rawArgs.length > 0) {
+      let args = []
+      rawArgs.forEach((arg, i) => {
+        if (types[i] === 'R') {
+          args.push('V' + arg.toString(16))
+        } else if (types[i] === 'N' || types[i] === 'NN' || types[i] === 'A' || types[i] === 'NN') {
+          args.push('0x' + arg.toString(16))
+        } else if (
+          types[i] === 'K' ||
+          types[i] === 'F' ||
+          types[i] === 'I' ||
+          types[i] === '[I]' ||
+          types[i] === 'DT' ||
+          types[i] === 'B' ||
+          types[i] === 'ST'
+        ) {
+          args.push(types[i])
+        } else {
+          args.push(types[i] + '0x' + ' ' + arg.toString(16))
+        }
+      })
+      formatted = decodedInstruction.instruction.name + ' ' + args.join(', ')
     } else {
       formatted = decodedIntruction.instruction.name
     }
+
     return formatted
   }
 
   dump(data) {
-    let lines = []
-
-    for (let i = 0; i < data.length; i++) {
+    let lines = data.map((code, i) => {
       let address = (i * 2).toString(16).padStart(6, '0')
-      let opcode = data[i].toString(16).padStart(4, '0')
-      let operands = this.format(this.disassemble(data[i]))
+      let opcode = code.toString(16).padStart(4, '0')
+      let operands = this.format(this.disassemble(code))
 
-      lines.push(`${address}  ${opcode}  ${operands}`)
-    }
+      return `${address}  ${opcode}  ${operands}`
+    })
 
     return lines.join('\n')
   }
