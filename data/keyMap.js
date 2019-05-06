f5f35c7684ddb1d578860e7097f0ed470504bc55
Tania Rascia <taniarascia@gmail.com>
2019-05-06 16:48:09 -0500

    convert tabs to spaces in keyMap comment

--- data/keyMap.js
+++ data/keyMap.js
@@ -1,8 +1,8 @@
 /**
-1	2	3	4
-Q	W	E	R
-A	S	D	F
-Z	X C	V	
+ 1 2 3 4
+ Q W E R
+ A S D F
+ Z X C V 
 */
 
 const keyMap = ['1', '2', '3', '4', 'q', 'w', 'e', 'r', 'a', 's', 'd', 'f', 'z', 'x', 'c', 'v']
