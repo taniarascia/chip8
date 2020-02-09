55d7f297933092dceab566310207c86db950d018
Tania Rascia <taniarascia@gmail.com>
2020-02-08 19:04:01 -0600

    Add instructions to web version

--- web/web.js
+++ web/web.js
@@ -2,6 +2,7 @@ let timer = 0
 
 function cycle() {
   timer++
+
   if (timer % 5 === 0) {
     cpu.tick()
     timer = 0
@@ -14,6 +15,7 @@ function cycle() {
   setTimeout(cycle, 3)
 }
 
+// Load a new ROM every time a new game is selected
 async function loadRom() {
   const rom = event.target.value
   const response = await fetch(`./roms/${rom}`)
@@ -23,6 +25,49 @@ async function loadRom() {
 
   cpu.interface.clearDisplay()
   cpu.load(romBuffer)
+  displayInstructions(rom)
+}
+
+function displayInstructions(rom) {
+  let instructions
+
+  switch (rom) {
+    case 'CONNECT4':
+      instructions = `Q = go left
+E = go right
+W = drop a coin
+
+The coin color alternates with each play. 
+This game has no win detection.
+`
+      break
+    case 'TETRIS':
+      instructions = `W = go left
+E = go right
+R = fall faster
+Q = rotate piece`
+      break
+    case 'PONG':
+      instructions = `Player 1:
+      
+2 = go up
+Q = go down
+
+Player 2:
+
+Z = go up
+X = go down`
+      break
+    case 'INVADERS':
+      instructions = `W = start game
+W = shoot
+Q = go left
+E = go right`
+      break
+  }
+
+  const instructionsDisplay = document.querySelector('.instructions')
+  instructionsDisplay.textContent = instructions
 }
 
 document.querySelector('select').addEventListener('change', loadRom)
